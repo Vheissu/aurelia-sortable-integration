1992f2341f2c0e365dde64af43946de305f493e3
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-09-22 11:27:22 +1000

    chore(build): added in autoprefixer for browser prefixes and CSS styling

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -8,6 +8,7 @@ var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 var notify = require("gulp-notify");
+var autoprefixer = require('gulp-autoprefixer');
 
 // transpiles changed es6 files to SystemJS format
 // the plumber() call prevents 'pipe breaking' caused
@@ -34,6 +35,9 @@ gulp.task('build-html', function () {
 gulp.task('build-css', function () {
   return gulp.src(paths.css)
     .pipe(changed(paths.output, {extension: '.css'}))
+	.pipe(autoprefixer({
+		browsers: ['> 5%', 'Explorer >= 10']
+    }))
     .pipe(gulp.dest(paths.output));
 });
 
